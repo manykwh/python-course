4700d87c043fcdd86d4240abb4da2417b416ada0
Jim Capozzoli <capojame@amazon.com>
2021-03-25 09:49:32 -0400

    Guide to Pythons Partition Function

---
+++ throwaway-variables-underscore.py
@@ -0,0 +1,14 @@
+heading = "Python: An introduction, and Python: Advanced"
+
+header, _, subheader = heading.partition(":")
+
+print(header)
+print(subheader)
+
+first, second, third = heading.partition(":")
+
+print(first)
+print(second)
+print(third)
+
+
