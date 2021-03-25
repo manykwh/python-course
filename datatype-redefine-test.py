ecbe7f7220d3091d1f1a41b1c42e49d717169f12
Jim Capozzoli <capojame@amazon.com>
2021-03-25 08:18:15 -0400

    How to Change Python Variable Values

---
+++ datatype-redefine-test.py
@@ -0,0 +1,6 @@
+x = 4
+print("Look our number is " + str(x))
+x = "4"
+print ("Look our number is " + x)
+# When ran from the command line (i.e., `python ./source.py`) we do not see the warning.  I'd have to change the level of verbosity of the interpreter, and I don't know how to do that yet because Cygwin doesn't have the man pages installed for my python here!
+
