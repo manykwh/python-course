f657a0519587fbf95c52bf5a2573ea448babe8e8
Jim Capozzoli <capojame@amazon.com>
2021-03-25 09:07:57 -0400

    Guide to heredocs in python

---
+++ heredoc.py
@@ -0,0 +1,10 @@
+content = """
+Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus eget urna mollis ornare vel eu leo.
+
+Vestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in.
+
+Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
+""".strip()
+
+print(content)
+
