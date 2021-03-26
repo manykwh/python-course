abcae1ec84b51fd79c6c1cdb9334074a2425f1f9
Jim Capozzoli <capojame@amazon.com>
2021-03-26 19:13:58 -0400

    updated my solution to be compatible with jordans

--- build-histogram.py
+++ build-histogram.py
@@ -1,6 +1,13 @@
 def build_histogram(values_dict):
     histogram_result = {}
     for k in values_dict.keys():
-        histogram_result[k] = int(values_dict[k]) * '$'
+        histogram_result[k[0]] = int(values_dict[k]) * '$'
     return histogram_result
+
+def print_histogram(values_dict):
+    for k in values_dict.keys():
+        print("{0}: {1}".format(k, values_dict[k]))
+
+sales = { 'google': 20, 'facebook': 42, 'twitter': 10, 'offline': 12, }
+print_histogram(build_histogram(sales))
 
