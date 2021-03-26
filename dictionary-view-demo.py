c65b508f7e64c529b9ea7236bc6f4a7cf9b9741f
Jim Capozzoli <capojame@amazon.com>
2021-03-26 18:51:57 -0400

    first demo of copying data-structures in python

---
+++ dictionary-view-demo.py
@@ -0,0 +1,21 @@
+players = {
+  "ss" : "Correa",
+  "2b" : "Altuve",
+  "3b" : "Bregman",
+  "DH" : "Gattis",
+  "OF" : "Springer",
+}
+
+player_names = list(players.copy().values())
+
+teams = {
+  "astros" : ["Altuve", "Correa", "Bregman"],
+  "angels":  ["Trout", "Pujols"],
+  "yankees": ["Judge", "Stanton"],
+  "red sox": ["Price", "Betts"],
+}
+
+team_groupings = teams.items()
+
+print(list(team_groupings)[1][1][0])
+
